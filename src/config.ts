42c525e21bdbb514880d01d46ecc6c462870a990
Steve Genoud <steve@sgenoud.com>
2024-04-09 15:33:34 +0200

    Small visual tweaks

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
   desc: "My 3D models",
   title: "My 3D models",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerPage: 6,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
